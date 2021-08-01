a2b36ddcac4e7e8a4e6191f4e4636efcb4289183
Jacob Arellano <jmm.arellano@gmail.com>
2021-08-01 14:24:09 -0600

    Add example for test types

--- spec/engine_spec.rb
+++ spec/engine_spec.rb
@@ -1,6 +1,21 @@
 require 'rspec'
-require './lib/engine'
+require './app/engine'
 
 RSpec.describe Engine do
+  before :each do
+    @engine = Engine.new
+  end
 
+  describe 'Happy paths' do
+    it 'it can turn on' do
+      expect(@engine.start).to eq(true)
+    end
+  end
+
+  describe 'Sad paths' do
+    it 'will not give error message is car is alread on' do
+      @engine.start
+      expect(@engine.start).to eq("Car is already on")
+    end
+  end
 end
